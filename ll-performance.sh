1d46d4f5c1921307e88e8c7ae3a46908667b14df
Ismail SEZEN <sezenismail@gmail.com>
2014-11-29 20:22:27 +0200

    minor improvements

--- ll-performance.sh
+++ ll-performance.sh
@@ -3,13 +3,14 @@
 # 2014-11-27
 # Compares performance of ll and native ls -l commands
 
-_path="/usr/bin"
-_n=50
-_ts_total_run_time=$(( $(date +%s%N)/1000000 ))
+_n=50 # number of runs
+_path="/usr/bin" # path to run
 
+_ts_total_run_time=$(( $(date +%s%N)/1000000 ))
 
 # Native ls performance
 _total=0
+_numoffiles=$(ls -1 "$_path" | wc -l)
 for i in $(seq 1 $_n);do
   _tsmp=$(( $(date +%s%N)/1000000 ))
   vcl=$(ls -l "$_path")
@@ -17,7 +18,8 @@ for i in $(seq 1 $_n);do
   _val=$(( $(date +%s%N)/1000000 - $_tsmp ))
   _total=$(( $_total + $_val ))
 done
-_lsperf=$(bc <<< "scale = 3; ($_total/$_n)")
+_lsruntime=$(bc <<< "scale = 3; ($_total/$_n)")
+_ls_perf_per_file=$(bc <<< "scale = 3; ($_lsruntime/$_numoffiles)")
 
 # ll performance
 _total=0
@@ -28,14 +30,17 @@ for i in $(seq 1 $_n);do
   _val=$(( $(date +%s%N)/1000000 - $_tsmp ))
   _total=$(( $_total + $_val ))
 done
-_llperf=$(bc <<< "scale = 3; ($_total/$_n)")
+_llruntime=$(bc <<< "scale = 3; ($_total/$_n)")
+_ll_perf_per_file=$(bc <<< "scale = 3; ($_llruntime/$_numoffiles)")
 
 # How much faster or slower?
-_comp_ll_ls=$(bc <<< "scale = 3; ($_llperf/$_lsperf)")
+_comp_ll_ls=$(bc <<< "scale = 3; ($_llruntime/$_lsruntime)")
 _ts_total_run_time=$(( $(date +%s%N)/1000000 - $_ts_total_run_time ))
+
 
 date
 echo "Total Run Time: $_ts_total_run_time ms."
-echo "pure ls Performance: $_lsperf ms."
-echo "ll Performance: $_llperf ms."
+echo "Number of files: $_numoffiles"
+echo "Pure ls run time: $_lsruntime ms. Perf: $_ls_perf_per_file"
+echo "ll run time: $_llruntime ms. Perf: $_ll_perf_per_file"
 echo "ll $_comp_ll_ls times slower than ls"
