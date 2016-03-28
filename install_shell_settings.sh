1922bb29353173949fad00efdaa39719a7e93040
İsmail SEZEN <sezenismail@gmail.com>
2016-03-28 06:08:41 +0300

    support for fish shell

--- install_shell_settings.sh
+++ install_shell_settings.sh
@@ -18,7 +18,7 @@ function save() {
   echo "Downloading script from ${url} and saving it to ${fname}..."
   curl -sL "${url}" > "${fname}" || die "Couldn't download script from ${url}"
   chmod +x "${fname}"
-  echo "Checking if ${SCRIPT} contains aliases..."
+  echo "Checking if ${SCRIPT} contains $fnamebase..."
   grep "$fnamebase" "${SCRIPT}" > /dev/null 2>&1 || (echo "Appending source command to ${SCRIPT}..."; echo "" >> "${SCRIPT}"; echo "test -e ${QUOTE}${relfname}${QUOTE} ${SHELL_AND} source ${QUOTE}${relfname}${QUOTE}" >> "${SCRIPT}")
   echo "Done."
   echo ""
@@ -29,12 +29,21 @@ SHELL=$(echo "${SHELL}" | tr / "\n" | tail -1)
 HOME_PREFIX='${HOME}'
 SHELL_AND='&&'
 QUOTE=''
-if [ "${SHELL}" == bash ]
-then
+if [ "${SHELL}" == bash ];then
   URL1="https://raw.githubusercontent.com/isezen/my-shell/master/alias.sh"
   URL2="https://raw.githubusercontent.com/isezen/my-shell/master/bash.sh"
   test -f "${HOME}/.bash_profile" && SCRIPT="${HOME}/.bash_profile" || SCRIPT="${HOME}/.profile"
   QUOTE='"'
+elif [ "${SHELL}" == fish ];then
+  echo "Make sure you have fish 2.2 or later. Your version is:"
+  fish -v
+
+  URL1="https://raw.githubusercontent.com/isezen/my-shell/master/alias.fish"
+  URL2="https://raw.githubusercontent.com/isezen/my-shell/master/bash.fish"
+  mkdir -p "${HOME}/.config/fish"
+  SCRIPT="${HOME}/.config/fish/config.fish"
+  HOME_PREFIX='{$HOME}'
+  SHELL_AND='; and'
 else
   die "Your shell, ${SHELL}, is not supported yet. Only bash are supported. Sorry!"
   exit 1
